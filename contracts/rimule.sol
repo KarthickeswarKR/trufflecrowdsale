0a96547cfd3d64a18ef1cebdb3f8258f87607984
Karthick Eswar <karthickeswar@virtualgodown.com>
2017-11-21 12:03:22 +0530

    interface

--- contracts/rimule.sol
+++ contracts/rimule.sol
@@ -4,27 +4,29 @@ import "../interface/contracts/crowdsale/CappedCrowdsale.sol";
 import "../interface/contracts/crowdsale/RefundableCrowdsale.sol";
 import "../interface/contracts/token/MintableToken.sol";
 
-contract CrowdsaleRimule is MintableToken {
+contract Rimule is MintableToken {
 
-  string public constant name = "RIMULE";
-  string public constant symbol = "RIM";
-  uint8 public constant decimals = 6;
+string public constant name = "RIMULE";
+string public constant symbol = "RIM";
+uint8 public constant decimals = 6;
 
 }
 
-contract Crowdsale is CappedCrowdsale, RefundableCrowdsale {
+contract phase1Crowdsale is CappedCrowdsale, RefundableCrowdsale {
 
-  function Crowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _goal, uint256 _cap, address _wallet)
+  function phase1Crowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _goal, uint256 _cap, address _wallet)
     CappedCrowdsale(_cap)
     FinalizableCrowdsale()
     RefundableCrowdsale(_goal)
     Crowdsale(_startTime, _endTime, _rate, _wallet)
   {
+    //As goal needs to be met for a successful crowdsale
+    //the value needs to less or equal than a cap which is limit for accepted funds
     require(_goal <= _cap);
   }
 
   function createTokenContract() internal returns (MintableToken) {
-    return new CrowdsaleRimule();
+    return new Rimule();
   }
 
 }
--- truffle.js
+++ truffle.js
@@ -1,9 +1,17 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var infura_apikey = "a571f826edd82755ff7242b8f2235e40";
+var mnemonic = "wise topple busy sound wide rich solve area toss where helmet pact";
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*"
-    }
+      network_id: "*" // Match any network id
+    },
+     ropsten:  {
+       provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
+            network_id: 3
+}
   }
 };
